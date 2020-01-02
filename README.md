5a3de15c4af4f3db7b41d51bbe04a757214f52b4
ViBrandao <vitor.ls.brandao@gmail.com>
2020-01-02 18:41:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lilv_music
+ 
---
+++ android/app/src/main/java/com/vitinho/lilv_music/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vitinho.lilv_music;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
